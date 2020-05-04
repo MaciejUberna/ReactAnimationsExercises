c0e5b498df297b91f9e32cb78f013d9b36d9731d
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-04 22:00:25 +0200

    Lesson402 "Wrapping the Transition Component" moved transition component to Modal.js and modified App.js so that it can work with Modal.js again.

--- src/App.js
+++ src/App.js
@@ -40,21 +40,12 @@ class App extends Component {
                           height: 100,
                           margin: 'auto',
                           transition: 'opacity 1s ease-out',
-                          opacity: state === 'exited' ? 1 : 0,
-                          opacity: state === 'entered' ? 1 : 0
+                          opacity: state === 'exited' ? 0 : 
+                          state === 'entered' ? 1 : 0
                         }}></div>
             )}
         </Transition>
-        <Transition 
-          in={this.state.modalIsOpen} 
-          timeout={300}
-          mountOnEnter
-          unmountOnExit
-        >
-          {state => (
-            <Modal show={state} closed={this.closeModal}/>
-          )}
-        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
