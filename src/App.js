2a20e1a9729a7b2071f0ff2d563ea8348927e5f3
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-04 20:05:13 +0200

    Lesson400 "Using ReactTransitionGroup" added opacity animation using 4 states of Transition group (Entering Entered Exiting Exited)

--- src/App.js
+++ src/App.js
@@ -27,8 +27,23 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className="Button" onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
         <br></br>
-        <Transition in={this.state.showBlock} timeout={300}>
-            {state => <p>{state}</p>}
+        <Transition
+          in={this.state.showBlock} 
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+            {state => (
+                        <div style={{
+                          backgroundColor: 'red',
+                          width: 100,
+                          height: 100,
+                          margin: 'auto',
+                          transition: 'opacity 1s ease-out',
+                          opacity: state === 'exited' ? 1 : 0,
+                          opacity: state === 'entered' ? 1 : 0
+                        }}></div>
+            )}
         </Transition>
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
