3a1ed4c0ad9b5197915d9488401ab39f2e7af6e6
Maciej Uberna <maciej.uberna@gmail.com>
2020-05-05 16:09:28 +0200

    Lesson404 "Transition Events" added events in Transision group in App.js that datect animation phase and can influence it. x6 onEnter, onEntering, onEntered, onExit, onExiting, onExited.

--- src/App.js
+++ src/App.js
@@ -32,9 +32,15 @@ class App extends Component {
           timeout={1000}
           mountOnEnter
           unmountOnExit
-        >
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+          >
             {state => (
-                        <div style={{
+              <div style={{
                           backgroundColor: 'red',
                           width: 100,
                           height: 100,
